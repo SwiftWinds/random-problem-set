96a2f38b76d0ec6c94a23b73954d84986c190c7b
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2020-09-14 02:16:20 -0700

    Fix bugs and example file

--- main.py
+++ main.py
@@ -1,49 +1,42 @@
-import itertools
 import random
 import click
+from functional import seq
 
 
 def ordinal(num):
-    "%d%s" % (num, "tsnrhtdd"[(num//10 % 10 != 1)*(num % 10 < 4)*num % 10::4])
+    return "%d%s" % (num, "tsnrhtdd"[(num//10 % 10 != 1)*(num % 10 < 4)*num % 10::4])
 
 
 def string_range_to_list(my_str):
-    print("The original string is : " + my_str)
     temp = [(lambda sub: range(sub[0], sub[-1] + 1))
             (list(map(int, ele.split('-')))) for ele in my_str.split(', ')]
     res = [b for a in temp for b in a]
-    print("List after converting it from string : " + str(res))
     return res
 
 
 @click.command()
-@click.option('--count', help='Number of random problems to generate.')
-@click.option('--file', type=click.Path(exists=True), help='Input file describing the problem set.')
+@click.option('--count', prompt='Number of problems you want',
+              help='Number of random problems to generate.', type=click.INT)
+@click.option('--file', prompt='File describing problem set',
+              help='Input file describing the problem set.', type=click.File())
 def generate_random_problems(count, file):
-    with open(file, 'r') as input_file:
-        click.echo(type(input_file))
-        s = input_file.read().splitlines()
+    """Simple program that generates COUNT random problems of a problem set described by FILE."""
+    chapters = file.read().splitlines()
 
-        result = list(map(lambda chapter: chapter.split(", ", 1), s))
+    problems = (seq(chapters)
+                .map(lambda chapter: chapter.split(": ", 1))
+                .map(lambda chapter: [chapter[0], string_range_to_list(chapter[1])])
+                .map(lambda chapter: [chapter[0],
+                                      seq(chapter[1])
+                                      .map(lambda problem: f"{chapter[0]} #{problem}")])
+                .map(lambda chapter: chapter[1])
+                .flatten()
+                .to_list())
 
-        click.echo(result)
-
-        result = list(map(lambda chapter: [chapter[0],
-                                           string_range_to_list(chapter[1])], result))
-
-        result = list(map(lambda chapter: [chapter[0], list(
-            map(lambda problem: chapter[0] + " " + str(problem), chapter[1]))], result))
-
-        result = list(map(lambda chapter: chapter[1], result))
-
-        merged = list(itertools.chain(*result))
-
-        result = []
-        for _ in itertools.repeat(None, count):
-            selected = random.choice(merged)
-            result += selected
-            click.echo("random item from list is: ", random.choice(selected))
-        return result
+    random_problem_set = random.sample(problems, count)
+    for idx, problem in enumerate(random_problem_set):
+        click.echo(f"{ordinal(idx + 1)} problem: {problem}")
+    return random_problem_set
 
 
 if __name__ == '__main__':
